af1067bbce29550403ff30051922f9995691df06
kuanhung <ericj.tw@gmail.com>
2017-05-03 20:37:03 +0800

    Update README and add gitconfig.sh

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
 # Usage
 
-* install vundle for vim
-```
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-```
-
 * clone this repository and install
 
 ```
-git clone git@github.com:ericj/my-env.git
+git clone https://github.com/ericj/my-env.git
 cd my-env && sh install.sh
 ```
 
-* in vim command mode, execute `:BundleInstall`
---
+++ gitconfig.sh
@@ -0,0 +1,18 @@
+# user
+git config --global user.email "ericj.tw@gmail.com"
+git config --global user.name "kuanhung"
+
+# alias
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.d difftool
+
+# editor
+git config --global core.editor /usr/bin/vim
+
+# difftool
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+git config --global alias.d vimdiff
