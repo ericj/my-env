0591af8edaaf76ea5efc7f596bb88c419a1319e2
kuanhung <ericj.tw@gmail.com>
2017-05-03 20:23:05 +0800

    Add vundle install and auto :BundleInstall

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+#set -x
 PWD=`pwd`
 
+if ! [ -d "$HOME/.vim/bundle/vundle" ]; then
+    echo "install vundle plugin for vim..."
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
+echo "creating vimrc soft link..."
 ln -sf $PWD/vimrc ~/.vimrc
+
+echo "vim :BundleInstall"
+vim +BundleInstall +qall!
