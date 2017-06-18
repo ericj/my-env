1f1f11ed1be7db5fbf8c334abd4af7ee82bf4ed9
kuanhung <ericj.tw@gmail.com>
2017-06-18 17:47:57 +0800

    Add install oh-my-zsh

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ ln -sf $PWD/vimrc ~/.vimrc
 
 echo "vim :BundleInstall"
 vim +BundleInstall +qall!
+
+echo "install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
