fd9a882523fe723af8a705f300958902ba0eaa21
kuanhung <ericj.tw@gmail.com>
2017-06-18 20:34:01 +0800

    Fix difftool alias

--- gitconfig.sh
+++ gitconfig.sh
@@ -7,7 +7,6 @@ git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.co checkout
 git config --global alias.ci commit
-git config --global alias.d difftool
 
 # editor
 git config --global core.editor /usr/bin/vim
@@ -15,4 +14,4 @@ git config --global core.editor /usr/bin/vim
 # difftool
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
-git config --global alias.d vimdiff
+git config --global alias.d difftool
