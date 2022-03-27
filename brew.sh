f29c1c64762742648680f3dac29e4eb67a4c25b0
kuanhung chen <ericj.tw@gmail.com>
2022-03-27 16:32:01 +0800

    Update brew pkg and zshrc

--- brew.sh
+++ brew.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 brew install vim openssl readline sqlite3 xz zlib git tcl-tk pyenv awscli automake \
-    shellcheck
+    shellcheck cmake youtube-dl ffmpeg tmux
 
+env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.10.3
 
-env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.10.0
+pip install aws-adfs
---
+++ update_zshrc.sh
@@ -0,0 +1,4 @@
+#pyenv init --path > ~/.zshrc
+#pyenv init - > ~/.zshrc
+
+echo 'export PATH="$HOME/bin:$PATH"' >> ~/.zshrc
