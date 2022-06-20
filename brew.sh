43f37f0e0aa78dae570b2b1e2bd7b5a2a86964e8
kuanhung chen <ericj.tw@gmail.com>
2022-06-20 22:05:17 +0800

    Update to python 3.10.4

--- brew.sh
+++ brew.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 brew install vim openssl readline sqlite3 xz zlib git tcl-tk pyenv awscli automake \
-    shellcheck cmake youtube-dl ffmpeg tmux
+    shellcheck cmake youtube-dl ffmpeg tmux ctags \
+    pipx pyenv-virtualenv bat
 
-env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.10.3
+env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.10.4
 
-pip install aws-adfs
+pipx install aws-adfs
